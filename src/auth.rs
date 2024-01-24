87b66ea0b23dcf4614c487c7b29998fa7164dd6c
Rohan Shukla <rdshukla1994@gmail.com>
2024-01-24 21:08:30 +0530

    Update db.rs

--- src/auth.rs
+++ src/auth.rs
@@ -1,5 +1,5 @@
 pub fn login(creds: models::Credentials) {
-    crate::database::get_user();
+    crate::database::get_user(121);
 }
 
 pub fn logout() {
--- src/database.rs
+++ src/database.rs
@@ -9,6 +9,18 @@ pub fn connect_db() -> Status {
     return Status::Connected;
 }
 
-pub fn get_user() {
+pub fn get_user(user_id: u32) -> Option<String> {
     //get user from db
+    let query = format!("GET username FROM users WHERE id={user_id}");
+
+    let db_result = query_db(query);
+    db_result.ok()
+}
+
+fn query_db(query: String) -> Result<String, String> {
+    if query.is_empty() {
+        Err(String::from("empty query provided"))
+    } else {
+        Ok(String::from("User from dB is!!!"))
+    }
 }
